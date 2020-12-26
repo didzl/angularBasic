15205e66b6c3dc68e7a3fc05471eb1b43b86c98c
didzl <delpiero1987@naver.com>
2020-12-26 12:49:43 +0900

    heroku first commit

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-//index.js
+// index.js
 
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.set('view engine','ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hello', function(req,res){
+  res.render('hello', {name:req.query.nameQuery});
 });
 
-var port = 3000;
-app.listen(port, function () {
-  console.log('server on! http://localhost:' + port);
-})
+app.get('/hello/:nameParam', function(req,res){
+  res.render('hello', {name:req.params.nameParam});
+});
+
+var port = process.env.PORT || 3000; // 1
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
+});
